8fece1cc50050cbadceaf8d9cde5570729a4e24c
m.schuetz <m.schuetz@mschuetz-PC>
2010-06-03 21:27:42 +0200

    initial java ee 6 tutorial

---
+++ src/test/java/de/akquinet/linuxTag2010/javaEE6Tutorial/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package de.akquinet.linuxTag2010.javaEE6Tutorial;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest
+{
+   @Test
+   public void testGetText() {
+      HelloWorld fixture = new HelloWorld();
+      assert "Hello World!".equals(fixture.getText());
+   }
+}
